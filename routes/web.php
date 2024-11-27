6dda2ec784de9f6c44851d90b89750be3d1e3c53
CherunoBread <qiyaanugrah75@gmail.com>
2024-11-27 16:38:38 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,6 @@ use App\Http\Controllers\SelectSeatController;
 route::post('/logout', [AuthController::class, 'logout']) -> name('logout');
 
 // Route untuk auth
-route::group(['prefix' => '/', 'middleware' => 'isGuest'], function(){
-    route::get('/', [AuthController::class, 'welcomeIndex'])->name('welcome');
-    route::get('/auth', [AuthController::class, 'authIndex'])->name('auth');
-    route::post('/login', [AuthController::class, 'login']);
-    route::post('/register', [AuthController::class, 'register']);
-    route::get('/register/otp', [AuthController::class, 'otpRegisterIndex'])->name('otp.form')->middleware('check.otp:auth');
-    route::post('/register/otp/verify', [AuthController::class, 'verify'])->name('otp.verify');
-});
 
 // Rute untuk lupa password
 route::group(['prefix' => '/forgot-password'], function(){

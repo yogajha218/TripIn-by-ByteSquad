b7c3021340b7eaf03957ec35d76f508badfcb3b9
radityadias <radityadias24@gmail.com>
2024-11-27 11:28:12 +0700

    add fade in effect for landing (template)

--- resources/js/Pages/LandingPage.jsx
+++ resources/js/Pages/LandingPage.jsx
@@ -1,17 +1,25 @@
 import { Link } from "@inertiajs/react";
+import { useEffect, useState } from "react";
+import '../../css/animation.css';
 
 const LandingPage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  
+  useEffect(() => {
+    const timer = setTimeout(()=> setIsVisible(true), 100);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-400 to-blue-100 flex flex-col items-center  justify-center px-20">
       {/* Logo Container */}
-      <div className="mb-2">
+      <div className={`mb-2 fade-in ${isVisible ? 'active' : ''}`}>
         <img src='/TripInLogo.svg'
          className="" alt="Logo of TripIn" />
       </div>
 
       {/* Text Container */}
-      <div className="text-center mb-12">
+      <div className={`text-center mb-12 fade-in ${isVisible ? 'active' : ''}`}>
         <h1 className="text-2xl font-semibold text-gray-800 mb-2">
           Enjoy Your Trip,
         </h1>

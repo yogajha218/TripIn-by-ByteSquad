73584a5d0014302ac0ab0ab448e4ece4fe818907
CherunoBread <qiyaanugrah75@gmail.com>
2024-12-17 17:24:56 +0700

    rebuild with security

--- public/build/assets/ButtonComponent-zdB5WSVn.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-CBGLgz3W.js";const i=({buttonText:t,disabled:e,onclick:m=()=>{},type:o})=>r.jsx("button",{type:o,onClick:m,className:"text-md w-full rounded-md bg-primary2 py-3 font-medium text-white active:bg-primary2/70 md:text-lg",disabled:e,children:t});export{i as B};

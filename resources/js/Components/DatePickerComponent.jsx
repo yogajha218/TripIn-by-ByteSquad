606297f8bbce4fed7c2ed88b8e4cb632d2327e63
yogajha218 <yogajha2108@gmail.com>
2024-12-17 16:00:09 +0700

    tiny adjustment on datepicker

--- resources/js/Components/DatePickerComponent.jsx
+++ resources/js/Components/DatePickerComponent.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
-import { Datepicker } from 'flowbite';
-import { format } from 'date-fns';
+import React, { useEffect } from "react";
+import { Datepicker } from "flowbite";
+import { format } from "date-fns";
 
 const DatePickerComponent = ({ setSelectedDay, data }) => {
     useEffect(() => {
-        const inputElement = document.querySelector('#default-datepicker');
+        const inputElement = document.querySelector("#default-datepicker");
         if (inputElement) {
             new Datepicker(inputElement, {
                 autohide: true,
@@ -28,24 +28,33 @@ const DatePickerComponent = ({ setSelectedDay, data }) => {
         <div>
             <label
                 htmlFor="default-datepicker"
-                className="block mb-2 text-sm font-medium text-gray-900"
+                className="mb-2 block text-sm font-medium text-gray-900"
             >
                 Select Date
             </label>
             <div className="relative max-w-sm">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3.5">
+                    <svg
+                        className="h-4 w-4 text-gray-500 dark:text-gray-400"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                    >
                         <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                     </svg>
                 </div>
                 <input
                     value={data.selectedDay}
-                    onChange={(e) => setSelectedDay('selectedDay', e.target.value)}
+                    onChange={(e) =>
+                        setSelectedDay("selectedDay", e.target.value)
+                    }
                     data-datepicker="true"
                     id="default-datepicker"
                     type="text"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                     placeholder="Select date"
+                    autocomplete="off"
                 />
             </div>
         </div>

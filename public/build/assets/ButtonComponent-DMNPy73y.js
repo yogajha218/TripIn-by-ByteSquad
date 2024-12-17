68187e2a4a4adde31fbae55ba1c22a26f7b70dbd
yogajha218 <yogajha2108@gmail.com>
2024-12-17 20:59:28 +0700

    build fix order detail

--- public/build/assets/ButtonComponent-DMNPy73y.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-2DLxi_6_.js";const s=({buttonText:t,disabled:e,onclick:o=()=>{},type:m})=>n.jsx("button",{type:m,onClick:o,className:"text-md w-full rounded-md bg-primary2 py-3 font-medium text-white md:text-lg",disabled:e,children:t});export{s as B};

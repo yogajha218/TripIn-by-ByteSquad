13436b381d6b2a5bc21aea43aadb21682e1719f3
radityadias <radityadias24@gmail.com>
2024-12-17 18:34:36 +0700

    final build v0.1

---
+++ public/build/assets/ButtonComponent-BG0lsDSB.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-uBn0CTW0.js";const s=({buttonText:t,disabled:e,onclick:o=()=>{},type:m})=>n.jsx("button",{type:m,onClick:o,className:"text-md w-full rounded-md bg-primary2 py-3 font-medium text-white md:text-lg",disabled:e,children:t});export{s as B};
